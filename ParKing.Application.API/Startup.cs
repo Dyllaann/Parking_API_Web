2ab5468f8a16317978361fb25059e83ffa926344
Dylan van Waarden <dylan.vanwaarden@gmail.com>
2018-11-21 15:30:37 +0100

    Add swagger ui

--- ParKing.Application.API/Startup.cs
+++ ParKing.Application.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ParKing.Application.API
 {
@@ -19,11 +20,23 @@ namespace ParKing.Application.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParKing");
+            });
+            
+            //Set develop settings
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -33,8 +46,18 @@ namespace ParKing.Application.API
                 app.UseHsts();
             }
 
+            //Setup MVC and HTTPS
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private static void AddSwagger(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ParKing API", Version = "v1" });
+            });
         }
     }
 }

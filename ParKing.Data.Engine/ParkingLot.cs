3e36d3034ae7cdddedfff72835ce37477fe2ef60
Dylan van Waarden <dylan.vanwaarden@gmail.com>
2018-11-27 13:56:08 +0100

    Add start on objects

---
+++ ParKing.Data.Engine/ParkingLot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParKing.Data.Engine
+{
+    public class ParkingLot
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string PricingZone { get; set; }
+        public bool HasChargingStation { get; set; }
+        
+
+        //EF Relations
+        public ParkingLocation Location { get; set; }
+    }
+}
